a254fef4bf72e4be7bb4e3b0cd6874f3ce6a2d94
LeadClaymore <ClaytonGormanRoss@gmail.com>
2024-10-18 17:27:33 -0400

    starting and deciding to use rapier for collisions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .run();
 }
